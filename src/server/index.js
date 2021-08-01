d4d55010eab6200d7169d31b64d3a86f2d5f915b
Rio Cantre <smittenaf301icu@gmail.com>
2021-08-01 17:28:47 +0200

    Added API and installed dotenv package

--- src/server/index.js
+++ src/server/index.js
@@ -1,9 +1,12 @@
 var path = require('path')
-const express = require('express')
 const mockAPIResponse = require('./mockAPI.js')
 const bodyParser = require('body-parser')
 const cors = require('cors');
+const express = require('express')
 const app = express()
+
+const dotenv = require('dotenv');
+dotenv.config();
 
 app.use(cors());
 app.use(bodyParser.urlencoded({extended: false}))
@@ -12,14 +15,18 @@ app.use(express.static('dist'))
 
 console.log(__dirname)
 
+const baseURL = 'https://api.meaningcloud.com/sentiment-2.1?'
+const apiKey = process.env.API_KEY
+
 app.get('/', function (req, res) {
     res.sendFile('dist/index.html')
 })
 
-
 app.get('/test', function (req, res) {
     res.send(mockAPIResponse)
 })
+
+
 
 // designates what port the app will listen to for incoming requests
 app.listen(8081, () => {
