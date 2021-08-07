0bbf2ddccff6dc5becf323fa29e61f8234f135a5
Rio Cantre <smittenaf301icu@gmail.com>
2021-08-07 18:42:21 +0200

    Refactor urlChecker

--- src/client/js/urlChecker.js
+++ src/client/js/urlChecker.js
@@ -1,11 +1,7 @@
 function checkForURL(inputURL) {
-    var regex = inputURL.match(/(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g);
-
-     if(regex == null){
-         return 0;
-     } else{
-     return 1;
-     }
+    const urlRegex = /(^http[s]?:\/{2})|(^www)|(^\/{1,2})/igm;
+    const url = new RegExp (urlRegex);
+    return url.test(inputURL);
  }
 
  export { checkForURL }
